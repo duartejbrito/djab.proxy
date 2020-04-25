3089554cebfc91daf942ec5eb4fde6bb6f2963a7
Duarte Brito <sytertzp@gmail.com>
2020-04-25 20:16:04 +0100

    .

--- djab.proxy/Program.cs
+++ djab.proxy/Program.cs
@@ -18,6 +18,10 @@ namespace djab.proxy
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((hostingContext, config) =>
+                {
+                    config.AddEnvironmentVariables();
+                })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
